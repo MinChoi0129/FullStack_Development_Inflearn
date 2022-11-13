38d58ce0b59295d85e8036c7f0801ddbdbc515f5
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-11-13 23:22:33 +0900

    antd design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
+import "antd/dist/antd.css";
 import "./App.css";
 import MainPageComponent from "./main";
 import UploadComponent from "./upload";
 import ProductComponent from "./product";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const history = useHistory();
+
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="/images/icons/logo.png" alt="" />
+          <Link to="/">
+            <img src="/images/icons/logo.png" alt="" />
+          </Link>
+          <Button
+            size="large"
+            onClick={() => {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품업로드
+          </Button>
         </div>
       </div>
+
       <div id="body">
         <Switch>
           <Route exact={true} path="/">
             <MainPageComponent />
           </Route>
+
           <Route exact={true} path="/products/:id">
             <ProductComponent />
           </Route>
+
           <Route exact={true} path="/upload">
             <UploadComponent />
           </Route>
         </Switch>
       </div>
+
       <div id="footer"></div>
     </div>
   );
--- src/product/index.js
+++ src/product/index.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./index.css"
 
 function ProductComponent() {
   const params = useParams();
@@ -25,15 +26,16 @@ function ProductComponent() {
   return (
     <div>
       <div id="image-box">
-        <img src={"/" + product.imageUrl} />
+        <img src={"/" + product.imageUrl} alt=""/>
       </div>
       <div id="profile-box">
-        <img src="/images/icons/avatar.png" />
+        <img src="/images/icons/avatar.png" alt=""/>
         <span>{product.seller}</span>
       </div>
       <div id="content-box">
         <div id="name">{product.name}</div>
-        <div id="price">{product.price}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2022.11.13</div>
         <div id="description">{product.description}</div>
       </div>
     </div>
