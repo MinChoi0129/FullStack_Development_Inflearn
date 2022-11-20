ecd8663a8c0975bbe9b8b3ada9baddbb83f72832
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-11-20 16:47:14 +0900

    edit index.css and html

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,5 +1,52 @@
+import { Form, Divider, Input } from "antd";
+import "./index.css";
 function UploadComponent() {
-    return <h1>업로드화면</h1>
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <Form name="상품 업로드" onFinish={onSubmit}>
+        <Form.Item name="upload">
+          <div id="upload-img-placeholder">
+            <img src="/images/icons/camera.png" alt="" />
+            <span>이미지를 업로드해주세요.</span>
+          </div>
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          label={
+            <div
+              className="upload-label"
+              name="seller"
+              rules={[
+                { required: true, message: "판매자 이름을 입력해주세요" },
+              ]}
+            >
+              판매자 명
+            </div>
+          }
+        >
+          <Input
+            className="upload-name"
+            size="large"
+            placeholder="이름을 입력해주세요"
+          />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="name"
+          label={<div className="upload-label">상품 이름</div>}
+          rules={[{ required: true, message: "상품 이름을 입력해주세요" }]}
+        >
+          <Input
+            className="upload-name"
+            size="large"
+            placeholder="상품 이름을 입력해주세요"
+          /> 
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
-
 export default UploadComponent;
